669069059bac9757c160690db9f6a042ce7fe3b0
MarienAngel16 <emary.presc16@gmail.com>
2025-11-22 18:51:27 -0600

    Inserción estructura, img y json

---
+++ js/app.js
@@ -0,0 +1,28 @@
+(function(){
+
+var app = angular.module('ejemplosApp',[ ]);
+
+
+/* $http es una inyección de instrucciones  */
+app.controller('mainCtrl', ['$scope','$http', function($scope,$http){
+  
+  $scope.autos= {  };
+
+$http.get('json/autos.json').success (function(data){
+//Código cuando es correcta la petición, va a mandar a llamar la data y la asigna 
+// a los profesores
+    $scope.autos = data.autos;
+
+});
+
+  
+
+
+
+}]);
+
+
+
+
+
+})();
